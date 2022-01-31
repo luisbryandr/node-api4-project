0cc3e89edaa6275c8e3400c34b900fc732846087
luisbryandr <luisbdominguezr@gmail.com>
2022-01-31 16:36:52 -0500

    wip

--- index.js
+++ index.js
@@ -4,13 +4,24 @@ const express = require('express');
 
 const server = express();
 
+// server.get('/api/users', (req, res) => {
+//     res.json([
+//         {id: 1, username: 'lalala'},
+//         {id: 2, username: 'lololo'},
+//         {id: 3, username: 'hohoho'}
+//     ])
+// })
 
-
-
-
-
-const PORT = process.env.PORT || 9000;
+const PORT = process.env.PORT || 8090;
 
 server.listen(PORT, () => {
     console.log(`Server listening on ${PORT}`)
+})
+
+server.get('/api/users', (req, res) => {
+    res.json([
+        {id: 1, username: 'lalala'},
+        {id: 2, username: 'lololo'},
+        {id: 3, username: 'hohoho'}
+    ])
 })
