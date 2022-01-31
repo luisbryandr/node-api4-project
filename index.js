91adeebe0166978313de1d45ce6511f6629baf34
luisbryandr <luisbdominguezr@gmail.com>
2022-01-31 15:53:16 -0500

    wip

---
+++ index.js
@@ -0,0 +1,16 @@
+require('dotenv').config();
+
+const express = require('express');
+
+const server = express();
+
+
+
+
+
+
+const PORT = process.env.PORT || 9000;
+
+server.listen(PORT, () => {
+    console.log(`Server listening on ${PORT}`)
+})
